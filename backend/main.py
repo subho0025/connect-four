6c0079b04bb2659b1bf26a75073257723cab0481
Subhajit <subhajitmaji0025@gmail.com>
2026-06-09 00:02:02 +0530

    updated CORS middleware to allow vercel fronted and also localhost origins

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,10 @@ app= FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://phantom-connect4.vercel.app",
+        "http://127.0.0.1:5500"
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
